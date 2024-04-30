416d6bf4d70f36ae44556328fbf0a361d7a2a762
Kamau-sam <skamau.6411@gmail.com>
2024-04-30 11:44:02 +0300

    Completed

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header({ handleMode, isDarkMode }) {
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={handleMode}>{isDarkMode ? "Dark" : "Light"} Mode</button>
+    </header>
+  );
+}
+
+export default Header;
